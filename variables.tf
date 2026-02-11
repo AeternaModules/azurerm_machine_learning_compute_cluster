3dae03c253e7f2c86961367dd1d0112fd51d6466
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:55:12 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -34,9 +34,9 @@ EOT
     vm_priority                   = string
     vm_size                       = string
     description                   = optional(string)
-    local_auth_enabled            = optional(bool, true)
-    node_public_ip_enabled        = optional(bool, true)
-    ssh_public_access_enabled     = optional(bool, false)
+    local_auth_enabled            = optional(bool) # Default: true
+    node_public_ip_enabled        = optional(bool) # Default: true
+    ssh_public_access_enabled     = optional(bool) # Default: false
     subnet_resource_id            = optional(string)
     tags                          = optional(map(string))
     scale_settings = object({
